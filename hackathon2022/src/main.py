1a7380ecbe0f0571f87e54c37249019c71b158f7
AmanSolanky <amansolanky24@gmail.com>
2022-09-17 01:57:04 +0530

    Commit_AK_Probability based on dependencies

--- hackathon2022/src/main.py
+++ hackathon2022/src/main.py
@@ -1,1 +1,20 @@
-import matplotlib.pyplot
+import csv
+import matplotlib.pyplot as plt
+from uiManager import UiManager
+from excelProcessing import csvPr
+csvprobj = csvPr()
+csvprobj.mappedDependencies()
+print(csvprobj.iterationMapping)
+#for i in range(1,(csvprobj.length[-1]-csvprobj.iterationMapping['M1'][-1])+1):
+#   csvprobj.iterationMapping['M1'].append(csvprobj.iterationMapping['M1'][-1])
+#plt.plot(csvprobj.length,csvprobj.iterationMapping['M1'])
+#plt.ylabel("Microservice")
+#plt.xlabel("Builds")
+#plt.title("Build vs Microservice failure")
+#plt.show()
+ui = UiManager(csvprobj)
+ui.mService = list(csvprobj.iterationMapping.keys())[2]
+ui.render()
+
+
+
